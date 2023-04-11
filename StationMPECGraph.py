691a79d74a9ddb343d7208a0306254d2ed7e37b2
Taegon Hibbitts <thibbitts03@gmail.com>
2023-04-10 23:35:37 -0400

    fixed MPEC urls

--- StationMPECGraph.py
+++ StationMPECGraph.py
@@ -25,7 +25,7 @@ TABLE station_XXX (observatory code):
     2 Observer    TEXT        List of observers as published in MPEC
     3 Measurer    TEXT        List of measurers as published in MPEC
     4 Facility    TEXT        List of telescope/instrument as published in MPEC
-    5 MPEC        TEXT        MPECId
+    5 MPEC        TEXT        MPECId1
     6 MPECType    TEXT        Type of the MPEC: Discovery, OrbitUpdate, DOU
     7 ObjectType    TEXT        Type of the object: NEA, Comet, Satellite, TNO, Unusual, Interstellar, unk
     8 Discovery    INTEGER        Corresponding to discovery asterisk
@@ -50,8 +50,25 @@ def printDict(someDictionary):
     for key,value in someDictionary.items():
         print("{}: {}".format(key,value))
 
-d = dict()
+BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+def encode(num, alphabet=BASE62):
+    """Encode a positive number in Base X
+    Arguments:
+    - `num`: The number to encode
+    - `alphabet`: The alphabet to use for encoding
+    """
+    if num == 0:
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        num, rem = divmod(num, base)
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
 def calcObs():
+    d = dict()
     cursor.execute("select * from MPEC")
     for mpec in cursor.fetchall():
         year = date.fromtimestamp(mpec[2]).year
@@ -121,34 +138,43 @@ def calcObs():
                 else:
                     temp.append("")
                 
-                #objects involved
-                #obj_type = cursor.execute("select ObjectType from station_{} where MPEC like {} and Time like {}".format(station, mpec[0], mpec[2])). fetchall()
-                #obj_type = cursor.execute("select ObjectType from station_{} where MPEC = {}".format(station, mpec[0]))
-                #obj_type = cursor.execute("select ObjectType from station_{}".format(station)).fetchall()
-                #print(len(obj_type))
-                #print(obj_type)
                 obj_type = mpec[7]
                 temp.append(obj_type)
-                url1 = "https://www.minorplanetcenter.net/mpec/{}/{}.html".format(station, mpec[0][-3::])
-                url_quoted = "\"{}\"".format(url1)
-                mpec_url = "<a href={}>{}</a>".format(url_quoted, station)
-                temp.append(mpec_url) #does the url contain the station and then the MPECID?
-                                      #MPECID to URL:
-                                      #proc.py
-                                      #A: rollover to 1-0
+                id = mpec[0][5::]
+                packed_front = ""
+                packed_back = ""
+
+                #packed front
+                if id[0:2] == "18":
+                    packed_front = "I" + id[2:4]
+                elif id[0:2] == "19":
+                    packed_front = "J" + id[2:4]
+                elif id[0:2] == "20":
+                    packed_front = "K" + id[2:4]
+                    
+                #packed_back
+                if len(id) == 8:
+                    packed_back = packed_front + id[-3::]
+                elif len(id) == 9:
+                    packed_back = packed_front + id[5] + encode(int(id[6:8])) + id[-1]
+                
+                url1 = "\"https://www.minorplanetcenter.net/mpec/{}/{}.html\"".format(packed_front, packed_back)
+                mpec_url = "<a href={}>Details</a>".format(url1)
+                temp.append(mpec_url)
                 d[station]['MPECs'].append((temp))
+    return d
     
 def main():
-    calcObs()
+    mpec_data = calcObs()
     includeFirstFU = True
     #for station_name in tableNames():
     for station_name in range(1):
         df = pd.DataFrame({"Year": [], "MPECType": [], "#MPECs": []})
         #station = station_name[0]
         station = 'station_010'
-        #print(d[station[-3::]]['MPECs'])
+        #print(mpec_data[station[-3::]]['MPECs'])
         ''' Prints the MPECs for each station
-        for MPEC in d[station[-3::]]['MPECs']:
+        for MPEC in mpec_data[station[-3::]]['MPECs']:
             print(MPEC)
         '''
         page = "C:\\Users\\taega\\Documents\\mpec_files\\WEB_Stations\\WEB_" + str(station) + ".html"
@@ -161,7 +187,9 @@ def main():
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.css" rel="stylesheet">
 
+        <script src="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.js"></script>
         <title>{}</title>
     </head>
     <body>
@@ -187,12 +215,11 @@ def main():
                     <th>Follow-Up</th>
                     <th>First Follow-Up</th>
                 </tr>
-            </table>
         """.format(station.capitalize(), station)
         
         for year in list(np.arange(1993, datetime.datetime.now().year+1, 1))[::-1]:
             obs_types = ["Editorial", "Discovery", "OrbitUpdate", "DOU", "ListUpdate", "Retraction", "Other", "Followup", "FirstFollowup"]
-            mpec_counts = list(map(lambda func: func(), [lambda mpecType=x: d[station[8::]][mpecType][year] if year in d[station[8::]][mpecType].keys() else 0 for x in obs_types]))
+            mpec_counts = list(map(lambda func: func(), [lambda mpecType=x: mpec_data[station[8::]][mpecType][year] if year in mpec_data[station[8::]][mpecType].keys() else 0 for x in obs_types]))
             if includeFirstFU:
                 mpec_counts[7] -= mpec_counts[8]
             else:
@@ -201,48 +228,59 @@ def main():
             df = pd.concat([df, pd.DataFrame({"Year": [year, year, year, year, year, year, year, year, year], "MPECType": ["Editorial", "Discovery", "OrbitUpdate", "DOU", "ListUpdate", "Retraction", "Other", "Followup", "FirstFollowup"], "#MPECs": mpec_counts})])
 
             o += """
-              <tr>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-                <td>%i</td>
-              </tr>
+                <tr>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                    <td>%i</td>
+                </tr>
             """ % (year, sum(mpec_counts), mpec_counts[0], mpec_counts[1], mpec_counts[2], mpec_counts[3], mpec_counts[4], mpec_counts[5], mpec_counts[6], mpec_counts[7], mpec_counts[8])
 
         o += """
-            <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+            </table>
+        </div>
+        <div class="toolbar">
+            <button id="load" class="btn btn-secondary">Load 10000 Rows</button>
+            <button id="append" class="btn btn-secondary">Append 10000 Rows</button>
+            Total rows: <span id="total"></span>
+        </div>
+            <table id="table" 
+                class="table table-striped table-bordered table-sm"
+                data-toolbar=".toolbar"
+                data-virtual-scroll="true"
+                data-show-columns="true">
                 <thead>
                     <tr>
-                        <th class="th-sm">Name
+                        <th class="th-sm" data-field="name">Name
 
                         </th>
-                        <th class="th-sm">Date/Time
+                        <th class="th-sm" data-field="date">Date/Time
 
                         </th>
-                        <th class="th-sm">DiscStation
+                        <th class="th-sm" data-field="ds">DiscStation
 
                         </th>
-                        <th class="th-sm">FirstConf
+                        <th class="th-sm" data-field="fs">FirstConf
 
                         </th>
-                        <th class="th-sm">Object
+                        <th class="th-sm" data-field="obj">Object
 
                         </th>
-                        <th class="th-sm">URL
+                        <th class="th-sm" data-field="url">URL
 
                         </th>
                     </tr>
                 </thead>
                 <tbody>
         """
-        for i in d[station[-3::]]['MPECs']:
+        for i in mpec_data[station[-3::]]['MPECs']:
             o += """
                     <tr>
                         <td>{}</td>
@@ -259,17 +297,59 @@ def main():
         o += """    
                 </tbody>
             </table>
-        </div>
+            <script>
+                var $table = $('#table')
+                var total = 0
+
+                function getData(number, isAppend) {
+                    if (!isAppend) {
+                    total = 0
+                    }
+                    var data = []
+                    for (var i = total; i < total + number; i++) {
+                    data.push({
+                        'name': i,
+                        'date': i,
+                        'ds': i,
+                        'fs': i,
+                        'obj': i,
+                        'url': i
+                    })
+                    }
+                    if (isAppend) {
+                    total += number
+                    } else {
+                    total = number
+                    }
+                    $('#total').text(total)
+                    return data
+                }
+
+                $(function() {
+                    $table.bootstrapTable({data: getData(20)})
+
+                    $('#load').click(function () {
+                    $table.bootstrapTable('load', getData(10000))
+                    })
+
+                    $('#append').click(function () {
+                    $table.bootstrapTable('append', getData(10000, true))
+                    })
+                })
+            </script>
+        <script src="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.js"></script>
     </body>
 </html>"""
         print(station)
         with open(page, 'w') as f:
             f.write(o)
 
-'''start = time.perf_counter()
-main()   c 
+'''
+start = time.perf_counter()
+main()
 finish = time.perf_counter()
-print('Time: ', finish-start)'''
+print('Time: ', finish-start)
+'''
 
 main()
 mpecconn.close()
